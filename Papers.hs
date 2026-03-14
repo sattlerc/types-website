88b51bf9b4b5b492eace2793d21deaeea3ab03ad
Christian Sattler <sattler.christian@gmail.com>
2026-03-14 22:34:07 +0100

    Transfer.

--- Papers.hs
+++ Papers.hs
@@ -5,13 +5,18 @@ import Prelude hiding (last)
 import Control.Monad ((>=>), forM_, unless, void)
 import Control.Arrow ((>>>))
 import Data.Aeson (FromJSON, eitherDecode, eitherDecodeFileStrict, fromJSON, parseJSON, withObject, (.:))
+import Data.Aeson.Types (Parser, Value)
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as ByteString
 import Data.Char (toLower)
-import Data.Function (on)
+import Data.Function ((&), on)
 import Data.List (intercalate, sort)
 import Data.Maybe (fromJust)
+import Data.Map (Map)
+import qualified Data.Map
 import Data.Text.Lazy (Text, unpack)
+import Data.Time (Day, TimeOfDay)
+import qualified Data.Time.Format.ISO8601 as ISO8601
 import GHC.Generics (Generic)
 
 import Text.Blaze.Html5 (Html)
@@ -19,6 +24,73 @@ import qualified Text.Blaze.Html5 as Blaze
 import qualified Text.Blaze.Html5.Attributes as Attributes
 import qualified Text.Blaze.Html.Renderer.Pretty as BlazePretty
 import qualified Text.Blaze.Html.Renderer.String as BlazeString
+
+-- Data.Time.Format.ISO8601
+
+-- Reading the JSON file of sessions
+
+data Session = Session
+  { session_id :: Integer
+  , session_title :: String
+  , session_papers :: [Integer]
+  } deriving (Eq, FromJSON, Generic, Show)
+
+instance Ord Session where
+  compare = compare `on` id
+
+parse_sessions :: FilePath -> IO (Map Integer Session)
+parse_sessions = ByteString.readFile >=> decode_json >>> fmap (map (\s -> (session_id s, s)) >>> Data.Map.fromList)
+
+
+type TimeOfDayRange = (TimeOfDay, TimeOfDay)
+
+--  { range :: TimeOfDayRange
+--  , 
+
+data Event = EventSpecial { event_special_title :: Text }
+           | EventBreak { event_break_title :: Text }
+           | EventSession { event_session_id :: Integer }
+  deriving (Eq, Show)
+
+newtype RangedEvent = RangedEvent { ranged_event :: (TimeOfDayRange, Event) }
+  deriving (Eq, Show)
+
+time_of_day_format = ISO8601.hourMinuteFormat ISO8601.ExtendedFormat
+
+time_of_day_parse :: (MonadFail m) => String -> m TimeOfDay
+time_of_day_parse = ISO8601.formatParseM time_of_day_format
+
+instance Ord RangedEvent where
+  compare = compare `on` ranged_event >>> fst
+
+parse_ranged_event :: Value -> Parser (TimeOfDayRange, Event)
+parse_ranged_event = withObject "RangedEvent" $ \v -> do
+  _from <- v .: "from" >>= time_of_day_parse
+  _to <- v .: "to" >>= time_of_day_parse
+  _type :: Text <- v .: "type"
+  event <- case _type of
+    "special" -> v .: "title" & fmap EventSpecial
+    "break" -> v .: "title" & fmap EventBreak
+    "session" -> v .: "id" & fmap EventSession
+    _ -> fail $ sc"unknown event type " ++ unpack _type
+  return $ RangedEvent ((_from, _to), event)
+
+day_format = ISO8601.hourMinuteFormat ISO8601.ExtendedFormat
+
+day_parse :: (MonadFail m) => String -> m TimeOfDay
+day_parse = ISO8601.iso8601ParseM time_of_day_format
+
+type Schedule = Map Day [(TimeOfDayRange, Event)]
+
+parse_schedule :: Value -> Parser Schedule
+parse_schedule = withObject "Schedule" $ do
+  days <- parse_days
+  
+  where
+  parse_day = do
+    _day <- v .: "day" >>= ISO8601.iso8601ParseM
+    ranged_events <- v .: "events"
+    return (_day, ranged_events)
 
 -- Reading the JSON file of papers.
 -- Includes basic string formatting.
