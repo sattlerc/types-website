78c021d3810d48cdaa79b9d05b2d14fe2f0fd9b6
Christian Sattler <sattler.christian@gmail.com>
2026-05-16 00:55:04 +0200

    Book of abstracts: use newpax (requires version 0.58).

--- BookOfAbstracts.hs
+++ BookOfAbstracts.hs
@@ -40,6 +40,9 @@ dir_book_of_abstracts_inverse = ".."
 path_book_of_abstracts_core :: FilePath
 path_book_of_abstracts_core = dir_book_of_abstracts </> "core.tex"
 
+file_newpax_generator :: FilePath
+file_newpax_generator = "newpax_generator.tex"
+
 gen_empty_line :: (Monad m) => LaTeXT_ m
 gen_empty_line = LaTeX.fromLaTeX $ LaTeX.TeXRaw "\n"
 
@@ -114,62 +117,71 @@ run_pax cwd path = do
   where
     trim = dropWhileEnd isSpace
 
-gen_entry :: (MonadIO m) => [Author] -> String -> FilePath -> LaTeXT_ m
+-- newpax v0.57 fails on Paper 16.
+-- Reported and fixed in v0.58.
+gen_newpax :: (Monad m) => [FilePath] -> LaTeXT_ m
+gen_newpax paths = do
+  LaTeX.documentclass [] "article"
+  LaTeX.fromLaTeX $ LaTeX.TeXComm "directlua" [LaTeX.FixArg $ fromString lua]
+  LaTeX.document $ return () where
+    lua_func_lit :: String -> String -> String
+    lua_func_lit function literal = function ++ "(" ++ show literal ++ ")"
+    
+    lua :: String
+    lua = unlines $ ["require(" ++ show "newpax" ++ ")"] ++ map (dropExtension >>> lua_func_lit "newpax.writenewpax") paths
+
+run_newpax :: [FilePath] -> IO ()
+run_newpax paths = do
+  LaTeX.execLaTeXT (gen_newpax paths) >>= LaTeX.renderFile (dir_book_of_abstracts </> file_newpax_generator)
+  run_latex "lualatex" dir_book_of_abstracts file_newpax_generator
+
+gen_entry :: (MonadIO m) => [Author] -> String -> FilePath -> WriterT [FilePath] (LaTeXT m) ()
 gen_entry authors title path = do
+  tell [rel_path]
   liftIO $ do
     let target = dir_book_of_abstracts </> rel_path
     copyFile path target
-    run_pax dir_book_of_abstracts rel_path
-    renameFile (replaceExtension target "pax") (replaceExtension target "newpax")
+    -- run_pax dir_book_of_abstracts rel_path
+    -- renameFile (replaceExtension target "pax") (replaceExtension target "newpax")
 
-  forM_ authors gen_index_author
-  gen_include_pdf title rel_path
-  gen_empty_line
+  lift $ do
+    forM_ authors gen_index_author
+    gen_include_pdf title rel_path
+    gen_empty_line
   where
     rel_path :: FilePath
     rel_path = takeFileName path
 
-gen_paper :: (MonadIO m) => Paper -> LaTeXT_ m
+gen_paper :: (MonadIO m) => Paper -> WriterT [FilePath] (LaTeXT m) ()
 gen_paper paper = gen_entry
   (paper_authors paper)
   (paper_title_latex_maybe paper)
   (fromJust $ paper_path paper)
 
-gen_invited :: (MonadIO m) => String -> Invited -> LaTeXT_ m
+gen_invited :: (MonadIO m) =>String -> Invited -> WriterT [FilePath] (LaTeXT m) ()
 gen_invited key invited = gen_entry
   [invited_author invited]
   (invited_title_latex_maybe invited)
   (path_invited_abstracts </> addExtension key "pdf")
 
-gen_book_of_abstracts :: (MonadIO m) => Papers -> Inviteds -> Sessions -> Schedule -> LaTeXT_ m
+gen_book_of_abstracts :: (MonadIO m) => Papers -> Inviteds -> Sessions -> Schedule -> WriterT [FilePath] (LaTeXT m) ()
 gen_book_of_abstracts papers inviteds sessions schedule = do
-  LaTeX.comment $ pack "Invited talks."
-  gen_empty_line
-
-  gen_section "Invited talks"
+  lift $ do
+    LaTeX.comment $ pack "Invited talks."
+    gen_empty_line
+    gen_section "Invited talks"
   forM_ (invited_key_by_schedule schedule) $ \key ->
     gen_invited key (inviteds Map.! key)
-  gen_empty_line
+  lift gen_empty_line
 
   forM_ (Map.assocs sessions) $ \(session_id, session) -> do
-    LaTeX.comment $ pack $ "Session " ++ show_id session_id ++ "."
-    gen_empty_line
-    gen_section $ update_head toUpper $ session_title session
+    lift $ do
+      LaTeX.comment $ pack $ "Session " ++ show_id session_id ++ "."
+      gen_empty_line
+      gen_section $ update_head toUpper $ session_title session
     forM_ (session_papers session) $ \paper_id -> do
       gen_paper $ papers Map.! paper_id
-    gen_empty_line
-
--- newpax fails on Paper 16.
--- gen_paxnew :: (Monad m) => [FilePath] -> LaTeXT_ m
--- gen_paxnew paths = do
---   LaTeX.documentclass [] "article"
---   LaTeX.fromLaTeX $ LaTeX.TeXComm "directlua" [LaTeX.FixArg $ fromString lua]
---   LaTeX.document $ return () where
---     lua_func_lit :: String -> String -> String
---     lua_func_lit function literal = function ++ "(" ++ show literal ++ ")"
-
---     lua :: String
---     lua = unlines $ ["require(" ++ show "newpax" ++ ")"] ++ map (dropExtension >>> lua_func_lit "newpax.writenewpax") paths
+    lift gen_empty_line
 
 generate :: IO ()
 generate = do
@@ -177,9 +189,10 @@ generate = do
   sessions <- parse_file_sessions Paths.sessions
   inviteds <- parse_file_inviteds Paths.inviteds
   schedule <- parse_file_schedule Paths.schedule
-  gen <- LaTeX.execLaTeXT $ gen_book_of_abstracts papers inviteds sessions schedule
   createDirectoryIfMissing True dir_book_of_abstracts
-  writeFile path_book_of_abstracts_core $ LaTeX.prettyLaTeX gen
+  (((), abstracts), latex) <- LaTeX.runLaTeXT $ runWriterT $ gen_book_of_abstracts papers inviteds sessions schedule
+  run_newpax abstracts
+  writeFile path_book_of_abstracts_core $ LaTeX.prettyLaTeX $ latex
 
 main :: IO ()
 main = do
